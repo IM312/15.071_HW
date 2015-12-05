a66c3e77ebf8a7b9e4cca107eaee808038c51ffc
IM312 <ianmkos@hotmail.com>
2015-12-05 15:16:30 -0600

    initial commit

---
+++ assignment3-2.R
@@ -0,0 +1,11 @@
+## edX MITx 15.071x The Analytics Edge
+## Assignment 3-2 Predicting Loan Repayment
+## Dataset --> loans.csv
+
+## Problem 1.1
+# Load the dataset and explore it
+loans = read.csv("loans.csv")
+str(loans)
+summary(loans)
+# What proportion of loans were not paid in full
+
