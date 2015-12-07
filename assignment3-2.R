c855e18df75dae8cfab921359e0662eb62804db9
ianmkos <ianmkos@hotmail.com>
2015-12-07 10:18:31 -0600

    problem 1 in progress

--- assignment3-2.R
+++ assignment3-2.R
@@ -11,6 +11,7 @@ summary(loans)
 table(loans$not.fully.paid)
 ## Problem 1.3
 # Build a data frame limited to observations with some missing data
+missing = subset(loans, is.na(log.annual.inc) | is.na(days.with.cr.line) | is.na(revol.util) | is.na(inq.last.6mths) | is.na(delinq.2yrs) | is.na(pub.rec))
 
 
 
