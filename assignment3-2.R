4eb71c464456c571c6f1640526ce3c10f34da738
ianmkos <ianmkos@hotmail.com>
2015-12-08 10:10:12 -0600

    switched to loans_imputed.csv dataset

--- assignment3-2.R
+++ assignment3-2.R
@@ -16,13 +16,13 @@ nrow(missing)
 table(missing$not.fully.paid)
 ## Problem 1.4
 # Use multiple imputation to fill in the missing data values
+install.packages("mice")
 library(mice)
 set.seed(144)
-# Set vars.for.imputation to all variables in the data frame except not.fully.paid, to impute the values using all of the 
-# other independent variables
+# Set vars.for.imputation to all variables in the data frame except not.fully.paid
 vars.for.imputation = setdiff(names(loans), "not.fully.paid")
 imputed = complete(mice(loans[vars.for.imputation]))
-loans = complete(mice(loans[vars.for.imputation]))
+loans[vars.for.imputation] = imputed
 summary(loans)
 # Test imputation results against provided imputation results (loans_imputed.csv)
 impResults = read.csv("loans_imputed.csv")
@@ -30,32 +30,39 @@ summary(impResults)
 
 ## Problem 2.1
 # Split the dataset into training and testing sets
-install.packages("caTools")
 library(caTools)
 # Set the random seed to 144
 set.seed(144)
 # Split so 70% of obs are for the training set
-spl= sample.split(loans$not.fully.paid, 0.7)
-Train = subset(loans, spl == TRUE)
-Test = subset(loans, spl == FALSE)
+spl = sample.split(impResults$not.fully.paid, 0.7)
+train = subset(impResults, spl = TRUE)
+test = subset(impResults, spl = FALSE)
 # Use logistig regression on the training set to predict the dependent variable
-mod1 = glm(not.fully.paid ~ ., data = Train, family = binomial)
+mod1 = glm(not.fully.paid ~ ., data = train, family = binomial)
 summary(mod1)
 ## Problem 2.3
 # Predict the probability of the test set loans not being paid back in full. Store the probabilities in 'predicted.risk' var
-Test$predicted.risk = predict(mod1, newdata = Test, type = "response")
+test$predicted.risk = predict(mod1, newdata = test, type = "response")
 # Compute the confusion matrix using a threshold of 0.5
 threshold = 0.5
-table(Test$not.fully.paid, as.numeric(Test$predicted.risk >= threshold))
+table(test$not.fully.paid, as.numeric(test$predicted.risk >= threshold))
 # Baseline model (using test set)
-table(Test$not.fully.paid)
+table(test$not.fully.paid)
 ## Problem 2.4
 # Use the ROCR package to compute the test set AUC
-install.packages("ROCR")
 library(ROCR)
-ROCRpred = prediction(Test$predicted.risk, Test$not.fully.paid)
+ROCRpred = prediction(test$predicted.risk, test$not.fully.paid)
 as.numeric(performance(ROCRpred, "auc")@y.values)
 
 ## Problem 3.2
+
+
+
+
+
+
+
+
+
 
 
