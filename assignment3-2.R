7b0416186bda6ff3a709259d979333af5e3c1261
IM312 <ianmkos@hotmail.com>
2015-12-07 22:25:38 -0600

    problem 3 in progress

--- assignment3-2.R
+++ assignment3-2.R
@@ -30,6 +30,31 @@ impResults = read.csv("loans_imputed.csv")
 summary(impResults)
 
 ## Problem 2.1
-## 
+# Split the dataset into training and testing sets
+library(caTools)
+# Set the random seed to 144
+set.seed(144)
+# Split so 70% of obs are for the training set
+spl = sample.split(loans$not.fully.paid, 0.7)
+Train = subset(loans, spl == TRUE)
+Test = subset(loans, spl == FALSE)
+# Use logistig regression on the training set to predict the dependent variable
+mod1 = glm(not.fully.paid ~ ., data = Train, family = binomial)
+summary(mod1)
+## Problem 2.3
+# Predict the probability of the test set loans not being paid back in full. Store the probabilities in 'predicted.risk' var
+Test$predicted.risk = predict(mod1, newdata = Test, type = "response")
+# Compute the confusion matrix using a threshold of 0.5
+threshold = 0.5
+table(Test$not.fully.paid, as.numeric(Test$predicted.risk >= threshold))
+# Baseline model (using test set)
+table(Test$not.fully.paid)
+## Problem 2.4
+# Use the ROCR package to compute the test set AUC
+library(ROCR)
+ROCRpred = prediction(Test$predicted.risk, Test$not.fully.paid)
+as.numeric(performance(ROCRpred, "auc")@y.values)
+
+## Problem 3.2
 
 
