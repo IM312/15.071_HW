a325e23c6807806b16255e02b21485e6d5310027
ianmkos <ianmkos@hotmail.com>
2015-12-10 11:28:00 -0600

    problem 2 complete

--- assignment2-2.R
+++ assignment2-2.R
@@ -11,6 +11,30 @@ subset(FluTrain, ILI==max(ILI))
 subset(FluTrain, Queries==max(Queries))
 
 ## Problem 1.2
+# Plot the histogram of the dependent variable, ILI
+hist(FluTrain$ILI)
+
+## Problem 1.3
+# Plot the natrual log of ILI versus Queries
+plot(FluTrain$Queries, log(FluTrain$ILI))
+
+## Problem 2.2
+# Run the regression model, FluTrend1 using: log(ILI)=intercept+coefficient*Queries
+FluTrend1 = lm(log(ILI) ~ Queries, data = FluTrain)
+# What is the R-squared value
+summary(FluTrend1)
+
+## Problem 2.3
+# What is the relationship we can infer between the independent and dependent variable
+correlation = cor(FluTrain$Queries, log(FluTrain$ILI))
+correlation^2
+log(1/correlation)
+exp(-0.5*correlation)
+# correlation^2 is equal to R-squared
+
+## Problem 3.1
+
+
 
 
 
