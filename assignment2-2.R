db5905886e7d3d1864588396f79a91a19832ee05
ianmkos <ianmkos@hotmail.com>
2015-12-10 14:05:22 -0600

    problem 3 in process

--- assignment2-2.R
+++ assignment2-2.R
@@ -33,6 +33,28 @@ exp(-0.5*correlation)
 # correlation^2 is equal to R-squared
 
 ## Problem 3.1
+# Load FluTest.csv
+FluTest = read.csv("FluTest.csv")
+# Predict the ILI value using FluTrend1 model. Note: need to convert log(ILI) to ILI
+PredTest = exp(predict(FluTrend1, newdata = FluTest))
+# What is the estimate of ILI-related physician visits for the week of March 11, 2012
+which(FluTest$Week == "2012-03-11 - 2012-03-17")
+PredTest[11] # Estimated ILI = 2.187378
+
+## Problem 3.2
+# What is the relative error between tbe estimate and the observed for that week
+# (Observed ILI - Estimated ILI)/Observed ILI
+FluTest$ILI[11] # Observed ILI = 2.293422
+(2.293422-2.187378)/2.293422
+
+## Problem 3.3
+
+
+
+
+
+
+
 
 
 
