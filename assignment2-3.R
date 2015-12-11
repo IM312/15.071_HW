3493774cb9e45283343040ee90cb318781a65f52
ianmkos <ianmkos@hotmail.com>
2015-12-11 09:10:17 -0600

    initial commit

--- assignment2-3.R
+++ assignment2-3.R
@@ -1,3 +1,8 @@
 ## edX MITx 15.071x The Analytics Edge
 ## Assignment 2-3 Reading Test Scores
 ## Dataset --> pisa2009train.csv & pisa2009test.csv
+
+## Problem 1.1
+# Load training and testing data sets
+pisaTrain = read.csv("pisa2009train.csv")
+pisaTest = read.csv("pisa2009test.csv")
