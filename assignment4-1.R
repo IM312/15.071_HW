aa455635e83bef9457f520daf2c2d2b96557e983
ianmkos <ianmkos@hotmail.com>
2015-12-17 14:38:54 -0600

    initial commit

---
+++ assignment4-1.R
@@ -0,0 +1,7 @@
+## edX MITx 15.071x The Analytics Edge
+## Assignment 4-1 Letter Recognition
+## Dataset --> letters_ABPR.csv
+
+## Problem 1.1
+# Load the file into R
+letters = read.csv("letters_ABPR.csv")
