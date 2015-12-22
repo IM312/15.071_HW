29e684dcd048e80eeac0f2d3701ecc4de1d63e8f
ianmkos <ianmkos@hotmail.com>
2015-12-22 11:49:26 -0600

    problem 1 in progress

--- assignment4-2.R
+++ assignment4-2.R
@@ -2,7 +2,21 @@
 ## Assignment 4-2 Predicting Earnings from Census Data
 ## Dataset --> census.csv
 
+## Libraries
+library(caTools)
+
 ## Problem 1.1
 # Load the dataset
 census = read.csv("census.csv")
 str(census)
+# Split the data
+spl = sample.split(census$over50k, SplitRatio = 0.6)
+set.seed(2000)
+train = subset(census, spl == TRUE)
+test = subset(census, spl == FALSE)
+# Build a logistic regression model using all of the independent variables to predict "over50k"
+mod1 = glm(over50k ~ ., data = train, family = binomial)
+# What variables are significant
+summary(mod1)
+
+## Problem 1.2
