5076fe8d8cb7c3bdb981b42c8d37201f6b920fa7
ianmkos <ianmkos@hotmail.com>
2015-12-22 10:02:18 -0600

    initial commit

---
+++ assignment4-2.R
@@ -0,0 +1,8 @@
+## edX MITx 15.071x The Analytics Edge
+## Assignment 4-2 Predicting Earnings from Census Data
+## Dataset --> census.csv
+
+## Problem 1.1
+# Load the dataset
+census = read.csv("census.csv")
+str(census)
