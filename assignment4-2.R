e8087c235ac27dd8e34dc2c5fb3bd6b9faaec26d
ianmkos <ianmkos@hotmail.com>
2015-12-22 15:58:26 -0600

    problem 1 complete

--- assignment4-2.R
+++ assignment4-2.R
@@ -4,6 +4,8 @@
 
 ## Libraries
 library(caTools)
+library(ROCR)
+
 
 ## Problem 1.1
 # Load the dataset
@@ -20,3 +22,32 @@ mod1 = glm(over50k ~ ., data = train, family = binomial)
 summary(mod1)
 
 ## Problem 1.2
+# What is the accuracy of the model on the testing set using a threshold of 0.5
+predictTest = predict(mod1, newdata = test, type = "response")
+table(test$over50k, predictTest > 0.5)
+accuracy = (9051+1888)/nrow(test)
+accuracy  # 0.855
+
+## Problem 1.3
+# What is the baseline accuracy for the testing set
+table(test$over50k)
+accuracy = 9713/nrow(test)
+accuracy
+
+## Problem 1.4
+# What is the AUC for this model on the test set
+ROCRpred = prediction(predictTest, test$over50k)
+auc = as.numeric(performance(ROCRpred, "auc")@y.values)
+auc  # 0.906
+#####################
+## Interpretation ##
+#####################
+# Given a random individual from the dataset who earned over $50k and a random indivdual from the dataset who did not earn
+# over $50k, the model will correctly classify which is which 90.6% of the time.
+
+## Problem 2.1
+
+
+
+
+
