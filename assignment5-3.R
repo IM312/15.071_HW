7aa90187040d17fdb3549b9e06a8154af8c3526f
ianmkos <ianmkos@hotmail.com>
2015-12-31 12:51:11 -0600

    initial commit

---
+++ assignment5-3.R
@@ -0,0 +1,9 @@
+## edX MITx 15.071x The Analytics Edge
+## Assignment 5-3 Separating Spam from Ham
+## Dataset --> emails.csv
+
+## Load libraries
+
+## Problem 1.1
+# Load the dataset
+emails = read.csv("emails.csv", stringsAsFactors = FALSE)
