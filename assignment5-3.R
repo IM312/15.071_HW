e96d02aab99730ce152fc8828f568553ecd0f44c
ianmkos <ianmkos@hotmail.com>
2015-12-31 14:56:32 -0600

    problem 2 in progress

--- assignment5-3.R
+++ assignment5-3.R
@@ -3,6 +3,9 @@
 ## Dataset --> emails.csv
 
 ## Load libraries
+library(tm)
+library(SnowballC)
+
 
 ## Problem 1.1
 # Load the dataset
@@ -29,4 +32,40 @@ which.min(nchar(emails$text))
 emails$text[1992]
 
 ## Problem 2.1
+# Preprocessing
+# Build a new corpus
+corpus = Corpus(VectorSource(emails$text))
+# Convert the text to lowercase
+corpus = tm_map(corpus, content_transformer(tolower)) # Wasn't getting the right # of terms w/o content_transformer added
+# Remove all punctuation from the corpus
+corpus = tm_map(corpus, removePunctuation)
+# Remove all English stopwords from the corpus
+corpus = tm_map(corpus, removeWords, stopwords("english"))
+# Stem the words in the corpus
+corpus = tm_map(corpus, stemDocument)
+# Build a document term matrix
+dtm = DocumentTermMatrix(corpus)
+
+# How many terms are in dtm
+str(dtm)
+
+## Problem 2.2
+# Limit dtm to contain terms appearing in at least 5% of documents
+spdtm = removeSparseTerms(dtm, 0.95)
+# How many terms are in spdtm
+str(spdtm)
+
+## Problem 2.3
+# Build a data frame called emailsSparse and use the make.names function to make the variable names of emailsSparse valid
+emailsSparse = as.data.frame(as.matrix(spdtm))
+colnames(emailsSparse) = make.names(colnames(emailsSparse))
+# What word stem shows up most frequently across all the emails
+which.max(colSums(emailsSparse))
+
+
+
+
+
+
+
 
