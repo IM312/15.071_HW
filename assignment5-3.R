85268d5a8e5567936202bf87d87ebd6bb571f9ac
ianmkos <ianmkos@hotmail.com>
2016-01-06 13:52:13 -0600

    problem 3 in progress

--- assignment5-3.R
+++ assignment5-3.R
@@ -5,6 +5,10 @@
 ## Load libraries
 library(tm)
 library(SnowballC)
+library(caTools)
+library(rpart)
+library(rpart.plot)
+library(randomForest)
 
 
 ## Problem 1.1
@@ -72,7 +76,58 @@ sort(colSums(subset(emailsSparse, spam == 0)))
 # How many stems appear at least 1000 times in the spam emails in the dataset
 sort(colSums(subset(emailsSparse, spam == 1)))
 
-## Problem 2.6
+## Problem 3.1
+# Convert the dependent variable to a factor
+emailsSparse$spam = as.factor(emailsSparse$spam)
+# Split emailsSparse 70/30 into a training set and testing set
+set.seed(123)
+split = sample.split(emailsSparse$spam, SplitRatio = 0.7)
+train = subset(emailsSparse, split == TRUE)
+test = subset(emailsSparse, split == FALSE)
+
+## Build machine learning models:
+# Logistic regression model
+spamLog = glm(spam ~ ., data = train, family = "binomial")
+# CART Model
+spamCART = rpart(spam ~ ., data = train, method = "class")
+# Random forest model
+set.seed(123)
+spamRF = randomForest(spam ~ ., data = train, method = "class")
+
+# For each model obtain the predicted spam probabilities for the training set
+predTrainCART = predict(spamCART)[,2]
+predTrainRF = predict(spamRF, type = "prob")[,2]
+predTrainLog = predict(spamLog, type = "response")
+
+###################
+## Error Message ##
+###################
+
+# glm.fit: algorithm did not converge
+# glm.fit: fitted probabilities numerically 0 or 1 occurred
+# These messages often indicate overfitting and the first indicates particularly severe overfitting
+
+# How many of the training set predicted probabilities from spamLog are <0.00001
+table(predTrainLog < 0.00001)  # 3046
+# How many of the training set predicted probabilities from spamLog are >0.99999
+table(predTrainLog > 0.99999)  # 954
+# How many of the taining set predicted probabilites from spamLog are 0.00001-0.99999
+table(predTrainLog >=0.00001 & predTrainLog <=0.99999)
+
+## Problem 3.2
+# How many variables are labeled as significant in the log reg model
+summary(spamLog)  # 0 (a symptom of the logistic regression algo not converging)
+
+
+## Problem 3.3
+
+
+
+
+
+
+
+
 
 
 
