beb1201631c97159c6beaf1882a49d1e751e8801
ianmkos <ianmkos@hotmail.com>
2016-01-13 11:18:06 -0600

    problem 2 in progress

--- assignment6-1.R
+++ assignment6-1.R
@@ -8,7 +8,7 @@
 # Read the data into R
 dailykos = read.csv("dailykos.csv")
 # Compute the distances
-kosDist = dist(dailykos [2:1546], method = "euclidean")
+kosDist = dist(dailykos [2:1546], method = "euclidean")  # Start at variable 2 to exclude "Document" variable
 # Use hclust to build the model
 kosHeirClust = hclust(kosDist, method = "ward.D")
 
@@ -40,6 +40,33 @@ tail(sort(colMeans(HeirClust1[-1])))
 # function outputs the last 6 words listed.
 
 ## Problem 1.6
+# Repeat the function above for the remaining clusters
+tail(sort(colMeans(HeirClust2[-1]))) 
+tail(sort(colMeans(HeirClust3[-1]))) 
+tail(sort(colMeans(HeirClust4[-1]))) 
+tail(sort(colMeans(HeirClust5[-1]))) 
+tail(sort(colMeans(HeirClust6[-1]))) 
+tail(sort(colMeans(HeirClust7[-1]))) 
+
+## Problem 2.1
+# Run k-means clustering, setting seed to 1000 with 7 clusters (exclude "Document" variable).
+set.seed(1000)
+KmeansCluster = kmeans(dailykos[2:1546], centers = 7)
+# Subset the data into 7 clusters using the "cluster" variable of the kmeans output
+KmeansCluster1 = subset(dailykos, KmeansCluster$cluster == 1)
+KmeansCluster2 = subset(dailykos, KmeansCluster$cluster == 2)
+KmeansCluster3 = subset(dailykos, KmeansCluster$cluster == 3)
+KmeansCluster4 = subset(dailykos, KmeansCluster$cluster == 4)
+KmeansCluster5 = subset(dailykos, KmeansCluster$cluster == 5)
+KmeansCluster6 = subset(dailykos, KmeansCluster$cluster == 6)
+KmeansCluster7 = subset(dailykos, KmeansCluster$cluster == 7)
+# How many observations are in Cluster 3
+# Which cluster has the fewest number of observations
+table(KmeansCluster$cluster)
+
+## Problem 2.2
+
+
 
 
 
