062fd9fc57775dd623c145e4f6cb345879384704
ianmkos <ianmkos@hotmail.com>
2016-01-13 09:25:16 -0600

    problem 1 in progress

--- assignment6-1.R
+++ assignment6-1.R
@@ -16,7 +16,7 @@ kosHeirClust = hclust(kosDist, method = "ward.D")
 # Plot the dendrogram
 plot(kosHeirClust)
 
-## Problem 1.3
+## Problem 1.4
 # Split the data into 7 clusters using the cutree function
 heirGroups = cutree(kosHeirClust, k = 7)
 # Use the subset function to subset the data by cluster
@@ -31,6 +31,15 @@ HeirClust7 = subset(dailykos, heirGroups == 7)
 # Which cluster has the most observations
 # Which cluster has the fewest observations
 table(heirGroups)
+
+## Problem 1.5
+# Look at the top 6 words in cluster 1
+tail(sort(colMeans(HeirClust1[-1]))) 
+# Computes the mean frequency values of each word in cluster 1. The [-1] removes the first column of HeirClust1. colMeans
+# computes the column (word) mean. The sort function orders the words in increasing order of the mean values. The tail
+# function outputs the last 6 words listed.
+
+## Problem 1.6
 
 
 
